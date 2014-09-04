4d34c6d36ee6f3ae733f8e6990bfc541144109a8
Hector <swreddit@gmail.com>
2014-09-04 02:17:25 +0100

    remove theSloth class, add GameBrain

---
+++ GameBrain.h
@@ -0,0 +1,13 @@
+//
+//  GameBrain.h
+//  AdventuresOfSloth
+//
+//  Created by Hector Janse van Rensburg on 04/09/2014.
+//  Copyright (c) 2014 S. Watercolour. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameBrain : NSObject
+
+@end
